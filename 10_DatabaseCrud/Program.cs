cffa05a5276b0d8f8444ea0ff6a39c1de39428f0
RidvanSerbes <ridvanserbes34@gmail.com>
2026-01-15 00:45:57 +0300

    -

---
+++ 10_DatabaseCrud/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_DatabaseCrud
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("****** Menü Sipariş İşlem Paneli ******");
+            Console.WriteLine();
+            
+            Console.WriteLine("--------------------");
+            #region Kategori Ekleme İşlemi
+            //Console.Write("Eklemek istediğiniz KATEGORİ Adı");
+            //string  categoryName=Console.ReadLine();
+
+            //SqlConnection connection = new SqlConnection("Data Source=localhost\\SQLEXPRESS;initial Catalog=EgitimKampiDb;integrated security=true");
+
+            //connection.Open();
+
+            //SqlCommand command = new SqlCommand("insert into TblCategory(CategoryName) values (@p1)",connection);
+            //command.Parameters.AddWithValue("@p1",categoryName);
+            //command.ExecuteNonQuery();
+            //connection.Close();
+
+
+            //Console.WriteLine("Kategori başarıyla eklendi");
+            #endregion
+
+            #region Ürün Ekleme İşlemi
+
+            //string productName;
+            //decimal productPrice;
+            ////bool productStatus;
+
+            //Console.Write("Eklemek istediğiniz ürün adı: ");
+            //productName=Console.ReadLine();
+            //Console.Write("Ürün Fiyatı: ");
+            //productPrice=decimal.Parse(Console.ReadLine());
+
+
+            //SqlConnection connection = new SqlConnection("Data source=localhost\\SQLEXPRESS;initial catalog=EgitimKampiDb;integrated security=true");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("insert into TblProduct(ProductName,ProductPrice,ProductStatus) values (@productName,@productPrice,@productStatus)",connection);
+            //command.Parameters.AddWithValue("@productName",productName);
+            //command.Parameters.AddWithValue("@productPrice",productPrice);
+            //command.Parameters.AddWithValue("@productStatus",true);
+            //command.ExecuteNonQuery();
+            //connection.Close();
+
+
+            //Console.Write("Ürün eklemesi başarılı");
+
+
+            #endregion
+
+            #region Ürün Listeleme İşlemi
+            //SqlConnection connection = new SqlConnection("Data Source=localhost\\SQLEXPRESS;initial Catalog=EgitimKampiDb;integrated security=true");
+            //connection.Open();
+
+            //SqlCommand command= new SqlCommand("Select * From TblProduct",connection);
+            //SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+            //DataTable dataTable = new DataTable();
+            //adapter.Fill(dataTable);
+
+            //foreach (DataRow row in dataTable.Rows) 
+            //{
+            //    foreach (var item in row.ItemArray) 
+            //    { 
+            //        Console.Write(item.ToString()+" ");
+
+            //    }
+            //    Console.WriteLine();
+
+            //}
+
+
+
+            //connection.Close();
+
+
+            #endregion
+
+            #region Ürün Silme İşlemi
+            //Console.Write("Silinecek ürün Id: ");
+            //int productId=int.Parse(Console.ReadLine());
+
+            //SqlConnection connection = new SqlConnection("Data Source=localhost\\SQLEXPRESS;initial Catalog=EgitimKampiDb;integrated security=true");
+            //connection.Open();
+
+            //SqlCommand command = new SqlCommand("Delete From TblProduct Where ProductId=@productId",connection);
+            //command.Parameters.AddWithValue("@productId",productId);
+            //command.ExecuteNonQuery();
+
+
+
+
+            //connection.Close();
+
+            //Console.WriteLine("Silme işlemi başarılı");
+
+
+            #endregion
+
+            
+            #region Ürün Güncelleme İşlemi
+            
+            //Console.Write("Güncellemek istediğin ürünün Id'si giriniz: ");
+            //int productId=int.Parse(Console.ReadLine());
+
+            //Console.Write("Güncellemek istediğin ürünün ismini giriniz: ");
+            //string productName = Console.ReadLine();
+
+            //Console.Write("Güncellemek istediğin ürünün fiyatını giriniz: ");
+
+            //decimal productPrice = decimal.Parse(Console.ReadLine());
+
+
+            //SqlConnection connection = new SqlConnection("Data Source=localhost\\SQLEXPRESS;initial Catalog=EgitimKampiDb;integrated security=true");
+            //connection.Open();
+
+            //SqlCommand command = new SqlCommand("Update TblProduct Set ProductName=@productName,ProductPrice=@productPrice where ProductId=@productId", connection);
+            //command.Parameters.AddWithValue("@productName", productName);
+            //command.Parameters.AddWithValue("@productPrice", productPrice);
+            //command.Parameters.AddWithValue("@productId", productId);
+            //command.ExecuteNonQuery();
+
+            //Console.WriteLine("Güncelleme başarılı");
+            //connection.Close();
+
+            #endregion
+
+            Console.Read();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
